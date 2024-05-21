0fa18daf81aa7d3c1087d7263849c772ba28858f
maruixiang@sina.com <maruixiang@sina.com>
2024-05-21 10:20:44 +0800

    etl

--- src/main/java/com/github/easy30/easymybatis/EntityTypeHandler.java
+++ src/main/java/com/github/easy30/easymybatis/EntityTypeHandler.java
@@ -3,6 +3,15 @@ package com.github.easy30.easymybatis;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.type.TypeHandlerRegistry;
 
+/**
+ * convert between entityClass props and database columns
+ */
 public interface EntityTypeHandler {
+    /**
+     * find props of entityClass which not register and register it;
+     * @param configuration
+     * @param registry
+     * @param entityClass
+     */
     void register(Configuration configuration, TypeHandlerRegistry registry, Class entityClass);
 }
